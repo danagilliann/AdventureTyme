625c88ae5955aff3ef8b0abdccdd3d23dde20c79
danagilliann <danalee96@gmail.com>
2015-09-20 04:09:34 -0400

    ugh igh ack

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname + '/views/index.html'));
+	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.get('/create', function(req, res) { 
